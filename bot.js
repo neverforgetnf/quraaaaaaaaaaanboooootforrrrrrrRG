cbf491486482c796ed8013751f83b96111fd073a
neverforgetnf <41763030+neverforgetnf@users.noreply.github.com>
2018-11-15 00:00:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -296,31 +296,4 @@ client.on("message", message => {
  
 	    
 });
-
-
-//////////////////////////////////////
-const adminprefix = ".";
-const devs = ['456641975932813345'];
-client.on('message', message => {
-  var argresult = message.content.split(`).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'sg')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
-} else 
-  if (message.content.startsWith(adminprefix + 'name')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'avt')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
-      } else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}`)
-}
-});
 client.login(process.env.BOT_TOKEN);
